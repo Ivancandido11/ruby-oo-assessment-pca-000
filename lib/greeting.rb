87580f1770d224b83df4f3649c6fbe4f2cc58ef1
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-16 10:50:47 -0400

    Completed up to Grandma

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -11,3 +11,46 @@
 # afternoon: 12 - 19: Good Afternoon!
 # night: 20 - 05: Good night
 
+class Greeting
+
+  attr_accessor :time
+
+  def initialize(time)
+    @time = time
+  end
+
+  def morning? 
+    if @time >= 6 && @time <= 11
+      true
+    else
+      false
+    end
+  end
+
+  def afternoon? 
+    if @time >= 12 && @time <= 19
+      true
+    else
+      false
+    end
+  end
+
+  def night? 
+    if @time <= 5 || @time >= 20
+      true
+    else
+      false
+    end
+  end
+
+  def say
+    if morning?
+      "Good Morning!"
+    elsif afternoon?
+      "Good Afternoon!"
+    elsif night?
+      "Good Night!"
+    end
+  end
+
+end
--- lib/meal.rb
+++ lib/meal.rb
@@ -2,3 +2,17 @@
 
 # Meal should accept an optional initialization argument that specifies
 # the meal choice. The default value for that argument should be meat.
+
+class Meal
+
+  attr_accessor :meal
+
+  def initialize(meal = "meat")
+    @meal = meal
+  end
+
+  def choice
+    @meal
+  end
+  
+end
